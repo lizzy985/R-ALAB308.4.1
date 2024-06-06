ec330ca90e26929b2c006c0ea8785790eb877f1e
Li Yang <lizzy.yang985@gmail.com>
2024-06-06 09:32:40 -0700

    My second commit

--- part1.js
+++ part1.js
@@ -1,29 +1,19 @@
 // ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26
-// 
-users = [
-    {
-        ID: 42,
-        Name:  "Bruce",
-        Occupation: "Knight",
-        Age: 41
-    },
-    {
-        ID: 57,
-        Name:  "Bob",
-        Occupation: "Fry Cook",
-        Age: 19 
-    },
-    {
-        ID: 63,
-        Name:  "Blaine",
-        Occupation: "Quiz Master",
-        Age: 58 
-    },
-    {
-        ID: 98,
-        Name:  "Bill",
-        Occupation: "octor's Assistant",
-        Age: 26
-    }
-]
+// ID,Name,Occupation,Age
+// 42,Bruce,Knight,41
+// 57,Bob,Fry Cook,19
+// 63,Blaine,Quiz Master,58
+// 98,Bill,Doctor’s Assistant,26
+let str = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26";
+
+let newArray = [];
+let rows = str.split("\n");
+let cell = "";
+rows.forEach(element => {
+    cell = element.split(",")
+    newArray.push(cell);
+});
+
+newArray.forEach(array => console.log(array[0], array[1], array[2], array[3]));
+
 
--- part2.js
+++ part2.js
@@ -1,7 +1,7 @@
 const str = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,QuizMaster,58\n98,Bill,Doctor's Assistant,26";
 let newArray = [];
 let row = [];
-let j = 0;
+// let j = 0;
 let cell = "";
 for(let i = 0; i < str.length; i++) {
     if(str[i] === ",") {
